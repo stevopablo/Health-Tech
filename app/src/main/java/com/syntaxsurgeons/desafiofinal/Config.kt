ec581baa354daea12fd5cb3a3de379a523418cfb
stevopablo <stevaopabllo@gmai.com>
2024-10-30 10:45:21 -0300

    user

--- app/src/main/java/com/syntaxsurgeons/desafiofinal/Config.kt
+++ app/src/main/java/com/syntaxsurgeons/desafiofinal/Config.kt
@@ -1,6 +1,8 @@
 package com.syntaxsurgeons.desafiofinal
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,13 @@ class Config : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val homeBtn = findViewById<ImageView>(R.id.homeBtn)
+        homeBtn.setOnClickListener {
+            val intent = Intent(this@Config, home::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
